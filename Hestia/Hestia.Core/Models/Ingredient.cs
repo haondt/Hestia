60dfd8185c5fece0735d4523b0daa764ca30b41e
haondt <code@mail.haondt.dev>
2025-07-21 15:46:30 -0600

    added ingredients list ui

--- Hestia/Hestia.Core/Models/Ingredient.cs
+++ Hestia/Hestia.Core/Models/Ingredient.cs
@@ -5,5 +5,15 @@
         public int Id { get; set; }
 
         public string Name { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+        public decimal? CaloriesPerUnit { get; set; }
+        public decimal? ProteinPerUnit { get; set; }
+        public decimal? CarbsPerUnit { get; set; }
+        public decimal? FatPerUnit { get; set; }
+        public string? Unit { get; set; }
+        public decimal? CostPerUnitInDollars { get; set; }
+
+        public decimal? UnitsPerServing { get; set; }
+        public decimal? UnitsPerPurchase { get; set; }
     }
 }
